dc797cf3632c60d951fddf8d17d2cf5006d07a8b
Rasim Aghayev <rasimaqayev@gmail.com>
2022-07-02 18:05:52 +0400

    1.File Upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FileUploadController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/upload-file', [FileUploadController::class, 'createUploadForm']);
+Route::post('/upload-file', [FileUploadController::class, 'fileUploadForm'])->name('fileUploadForm');
